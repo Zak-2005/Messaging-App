6805eb0420e8b7655dfbaa9e91ef6d4a214a9d08
Zak-2005 <zak.mob2005@gmail.com>
2023-08-02 19:19:01 -0500

    Create express server and serve index.html

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
-const express = require("express");
+const express = require("express")
+const path = require("path")
 const app = express();
-
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT => console.log("Server connected to port: ", PORT))
+app.use('/', express.static(path.join(__dirname, "public")))
+
+app.use('/',  require("./routes/root"))
+
+
+
+app.listen(PORT, () => {console.log("Server connected to port: ", PORT)})
