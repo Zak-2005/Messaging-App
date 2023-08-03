ac3884d38475e7f571f8f9f52d22e86e68c2971c
Zak-2005 <zak.mob2005@gmail.com>
2023-08-02 19:24:16 -0500

    add necessary routes and controller files

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 3500;
 
 app.use('/', express.static(path.join(__dirname, "public")))
 
+//routes
 app.use('/',  require("./routes/root"))
-
+//app.use('/register', require('./routes/register'))
+//app.use('/login', require('./routes/login'))
+//app.use('/users', require('./routes/users'))
 
 
 app.listen(PORT, () => {console.log("Server connected to port: ", PORT)})
