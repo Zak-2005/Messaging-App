6a82d3c4e2465f37cfc10e8c43f7e7e9fc95e8ec
Zak-2005 <zak.mob2005@gmail.com>
2023-08-07 15:59:42 -0500

    add Automatically added to Main Chat when registered

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,24 +1,37 @@
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
-const registerNewUser = async(req,res)=> {
-    console.log(req.body)
-    const {user, pass} = req.body;
-    if(!user || !pass){
-        res.status(400).json({msg: "Username and password required"})
+const User = require("../models/User");
+const Chat = require("../models/Chat");
+const bcrypt = require("bcrypt");
+const registerNewUser = async (req, res) => {
+  console.log(req.body);
+  const { user, pass } = req.body;
+  if (!user || !pass) {
+    res.status(400).json({ msg: "Username and password required" });
+  }
+  const duplicate = await User.findOne({ username: user }).exec();
+  if (duplicate) return res.sendStatus(409);
+  try {
+    const hashedPwd = await bcrypt.hash(pass, 10);
+    console.log(User);
+    const createUser = await User.create({
+      username: user,
+      password: hashedPwd,
+    });
+    const mainChatExists = await Chat.findOne({ name: "Main Chat" }).exec();
+    if (!mainChatExists) {
+     const mainChat = await Chat.create({
+        name: "Main Chat",
+      });
+      mainChat.users.push(createUser.username);
+      await mainChat.save();
     }
-    const duplicate = await User.findOne({username: user}).exec()
-    if(duplicate) return res.sendStatus(409)
-    try{
-        const hashedPwd = await bcrypt.hash(pass, 10);
-        console.log(User)
-        const createUser = await User.create({
-            "username": user,
-            "password": hashedPwd
-        });
-        res.json({"success": true})
-    }catch(err){
-        res.status(500).json({msg: err.message})
+    else{
+        mainChatExists.users.push(createUser.username);
+        await mainChatExists.save();
     }
-}
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
 
-module.exports = {registerNewUser}
+module.exports = { registerNewUser };
