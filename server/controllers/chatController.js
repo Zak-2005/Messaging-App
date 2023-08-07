5c0047c7a34b02b46b731121dfcc2d627513c9be
Zak-2005 <zak.mob2005@gmail.com>
2023-08-07 15:57:59 -0500

    add Get Current Chat to chatController

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,13 +1,15 @@
 const Chat = require("../models/Chat");
 const User = require("../models/User")
 const handleNewChat = async (req, res) => {
-  const { chat } = req.body;
-  if(!chat) res.status(400).json({msg: "Please enter a chat field"})
+  const { chat, user } = req.body;
+  if(!chat || !user) return res.status(400).json({msg: "Please enter a chat and user field"})
+  const foundUser = await User.findOne({username:user})
+  if(!foundUser) return res.status(400).json({msg:"User does not exist"})
   try{
   const createChat = await Chat.create({
-    "name": chat
+    "name": chat,
+    "users": [user]
   })
-  console.log(createChat)
   res.json({msg: `Your chat: ${chat} was created successfully!`})
 }catch(err){
     console.error(err)
@@ -16,19 +18,26 @@ const handleNewChat = async (req, res) => {
 };
 
 const handleGetAllChats = async(req,res)=>{
-    const allChats = await Chat.find()
+    const allChats = await Chat.find().exec()
     res.json(allChats)
 
+}
+const handleGetCurrentChat = async(req,res)=>{
+    const {chat} =req.body
+    if(!chat) return res.status(400).json({msg:"Please include a chat name"})
+    const currentChat = await Chat.findOne({name:chat}).exec()
+    if(!currentChat) return res.status(400).json({msg:"Chat does not exist"})
+    return res.json(currentChat)
 }
 
 const handleAddFriendToChat = async(req,res) =>{
     const {chat, user} = req.body;
     if(!user) return res.status(400).json({msg:"Please enter a user to be added"})
-    const foundUser = await User.findOne({user:user}).exec()
+    const foundUser = await User.findOne({username:user}).exec()
     if(!foundUser) return res.status(400).json({msg:"User does not exist"})
     const chatExists = await Chat.findOne({name:chat}).exec()
     if(!chatExists) return res.status(400).json({msg:"Chat does not exist"})
-    const duplicate = chatExists.users.find(user)
+    const duplicate = chatExists.users.find((username)=>username===user)
     if(duplicate) return res.status(400).json({msg:"User already in chat"})
     try{
         chatExists.users.push(user)
@@ -39,4 +48,4 @@ const handleAddFriendToChat = async(req,res) =>{
         return res.status(500).json({msg: "Failed to add user to chat"})
     }
 }
-module.exports = {handleNewChat,handleGetAllChats,handleAddFriendToChat}
+module.exports = {handleNewChat,handleGetAllChats,handleAddFriendToChat,handleGetCurrentChat}
