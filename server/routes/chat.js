de9eb6334b807f5d2210ad534c14c2de35da0040
Zak-2005 <zak.mob2005@gmail.com>
2023-08-07 15:58:08 -0500

    add Get current chat route

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const chatController = require('../controllers/chatController')
 
 router.get('/',chatController.handleGetAllChats)
+router.post('/current', chatController.handleGetCurrentChat)
 router.post('/', chatController.handleNewChat)
 router.post('/addToChat',chatController.handleAddFriendToChat)
 module.exports = router
