d7c669fb7d303fd99bf52dcac7ade45b4bf5bee2
Zak-2005 <zak.mob2005@gmail.com>
2023-08-07 16:00:38 -0500

    remove uneccessary function

--- client/src/components/MainApp/Header.jsx
+++ client/src/components/MainApp/Header.jsx
@@ -2,16 +2,12 @@ import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
 import App from "../../App"
 import Register from "../Auth/Register"
 import '../../css/header.css'
-export default function Header({showUserList, currentChat}){
-
-    const showSettings = ()=>{
-        
-    }
+export default function Header({showUserList, currentChat, handleInviteToChat}){
     return(
         <div className="header">
             <h3>{currentChat}</h3>
             <div className='icon-holder'>
-                <img src="../../public/notif.png" alt="notification bell" className='icon'/>
+                <img src="../../public/invite.png" alt="invite to server" className='icon' onClick={()=>handleInviteToChat()}/>
                 <img src="../../public/users.png" alt="user list" className='icon' onClick={()=>showUserList()}/>
                 <Link to="/settings"><img src="../../public/settings.png" alt="settings" className='icon'/></Link>
             </div>
