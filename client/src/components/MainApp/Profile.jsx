44f439b1b0b5617bf6201ed65e468b6fc23c44c1
Zak-2005 <zak.mob2005@gmail.com>
2023-08-08 13:16:51 -0500

    add personal info to profile page

--- client/src/components/MainApp/Profile.jsx
+++ client/src/components/MainApp/Profile.jsx
@@ -1,22 +1,20 @@
 import "../../css/settings.css";
 
-export default function Profile() {
-    
+export default function Profile({foundUserInfo}) {
+
+
   return (
-    <div>
-      <img src="" alt="Picture" />
-      <div className="Profile">
-        <div>
-          <h3>Bio:</h3>
-          <p>Example Bio</p>
+    <div className="profileContainer">
+      <div className="profile">
+        <img src="../public/invite.png" alt="Picture" />
+
+        <div className="profile-element">
+          <h3 className="element-title">Username:</h3>
+          <p>{foundUserInfo?foundUserInfo.username:""}</p>
         </div>
-        <div>
-          <h3>Role:</h3>
-          <p>Role</p>
-        </div>
-        <div>
-        <h3>Username:</h3>
-          <p>Username</p>
+        <div className="profile-element">
+          <h3 className="element-title">Bio:</h3>
+          <p>{foundUserInfo.bio?foundUserInfo.bio:"No Bio"}</p>
         </div>
       </div>
     </div>
