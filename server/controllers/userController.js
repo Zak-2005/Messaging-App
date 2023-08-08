b1dd0d9a5641be3c5e4fd050321fbb2b08b14450
Zak-2005 <zak.mob2005@gmail.com>
2023-08-08 13:19:28 -0500

    allow request for other users and edit bio

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 const jwt = require("jsonwebtoken");
 
 
-const handleGetUser = async (req, res) => {
+const handleGetCurrentUser = async (req, res) => {
     console.log(req.cookies)
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
@@ -18,9 +18,31 @@ const handleGetUser = async (req, res) => {
   });
 };
 
+const handleGetOtherUser = async(req,res)=>{
+    const {user} = req.body
+    if(!user) return res.status(400).json({msg:"No user entered"})
+    const foundUser = await User.findOne({username:user}).exec()
+    if(!foundUser) return res.status(400).json({msg:"User does not exist"})
+    res.status(201).json(foundUser)
+}
 const handleGetAllUsers = async(req,res)=>{
     const allUsers = await User.find().exec();
     console.log(allUsers)
     res.json(allUsers)
 }
-module.exports = { handleGetUser,handleGetAllUsers };
+
+const handleEditBio = async(req,res)=>{
+    const {user,bio} = req.body
+    if(!user || !bio) res.status(400).json({msg:"Please enter a user and a bio"})
+    const foundUser = await User.findOne({username:user})
+    if(!foundUser) res.status(400).json({msg:"User does not exist"})
+    try{
+        foundUser.bio = bio;
+        await foundUser.save()
+        return res.status(200).json({msg:"Successfully updated bio"})
+    }catch(err){
+        console.error(err)
+        res.status(500).json({msg:"Failed to update bio"})
+    }
+}
+module.exports = { handleGetCurrentUser,handleGetAllUsers, handleGetOtherUser, handleEditBio};
