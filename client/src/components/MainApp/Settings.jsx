597a2c6851b210ba017f30ab05f2b05ad3dac632
Zak-2005 <zak.mob2005@gmail.com>
2023-08-08 13:17:07 -0500

    change settings to personal user profile

--- client/src/components/MainApp/Settings.jsx
+++ client/src/components/MainApp/Settings.jsx
@@ -1,20 +1,34 @@
 import "../../css/settings.css";
 import Profile from "./Profile";
 import {Link} from "react-router-dom"
+import {useEffect, useState} from 'react'
+import { useParams, } from "react-router-dom";
+import axios from "axios";
 export default function Settings() {
+  const user = useParams();
+  const [foundUserInfo, setFoundUserInfo] = useState(null)
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const userInfo = await axios.post("http://localhost:3500/user/otherUser", {
+        user: user.user,
+      });
+      setFoundUserInfo(userInfo.data);
+    };
+    getUserInfo()
+  });
   return (
     <div className="settingsContainer">
       <div className="settingsTitle">
-        <h1>Profile</h1>
+        <h1>{foundUserInfo?foundUserInfo.username:""}'s Profile</h1>
       </div>
 
       <div className="settingsSidebar">
         <ul className="settings">
           <li>Profile</li>
-          <Link to="/"><li>Logout</li></Link>
+          <Link to="/" className="logout"><li>Logout</li></Link>
         </ul>
       </div>
-      <Profile />
+      <Profile foundUserInfo={foundUserInfo?foundUserInfo:""}/>
       
     </div>
   );
