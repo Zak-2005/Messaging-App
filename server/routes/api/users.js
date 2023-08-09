213add25ab1e63c56e1b3683d6f841804630c877
Zak-2005 <zak.mob2005@gmail.com>
2023-08-09 16:11:38 -0500

    verify changes to profile with jwt

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,11 +2,11 @@ const express = require("express")
 const path = require('path')
 const router = express.Router();
 const userController = require('../../controllers/userController')
-
+const verifyJWT = require('../../controllers/verifyJWT')
 router.get('/',userController.handleGetAllUsers)
-router.post('/',userController.handleGetCurrentUser)
+router.post('/',verifyJWT,userController.handleGetCurrentUser)
 router.post('/otherUser',userController.handleGetOtherUser )
 router.put('/bio',userController.handleEditBio)
-router.put('/newUsername',userController.handleEditUsername)
-router.put('/newPass',userController.handleChangePass)
+router.put('/newUsername',verifyJWT,userController.handleEditUsername)
+router.put('/newPass',verifyJWT,userController.handleChangePass)
 module.exports = router;
