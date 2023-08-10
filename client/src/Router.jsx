cbefb562cc47eddcc60e875a6518a289169efdbc
Zak-2005 <zak.mob2005@gmail.com>
2023-08-10 16:56:54 -0500

    add 404 route

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,7 +3,7 @@ import App from "./App"
 import Register from "./components/Auth/Register"
 import Settings from "./components/MainApp/Settings"
 import Login from "./components/Auth/Login"
-import Profile from "./components/MainApp/Profile"
+import NotFound from "./components/404/NotFound"
 export default function Router(){
 
     return(
@@ -13,6 +13,7 @@ export default function Router(){
         <Route path="/login" element={<Login />}/>
         <Route path="/dashboard" element={<App />}/>
         <Route path="/:user" element={<Settings/>}/>
+        <Route path="/404" element={<NotFound />}/>
         </Routes>
         </BrowserRouter>
     )
