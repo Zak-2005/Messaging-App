fdc938e3b1d81f4cb480fd030764caad27f27166
Zak-2005 <zak.mob2005@gmail.com>
2023-08-10 17:25:53 -0500

    modify corsOptions

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -10,11 +10,7 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Enable sending cookies and credentials
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
     optionsSuccessStatus: 200
 }
 
