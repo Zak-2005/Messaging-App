efea8309a82cf9096c235b67f12c68555c26a5cb
Zak-2005 <zak.mob2005@gmail.com>
2023-08-10 16:53:59 -0500

    Finish settings page

--- client/src/components/MainApp/Settings.jsx
+++ client/src/components/MainApp/Settings.jsx
@@ -1,39 +1,60 @@
 import "../../css/settings.css";
 import Profile from "./Profile";
-import {Link} from "react-router-dom"
-import {useEffect, useState} from 'react'
-import { useParams, } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 export default function Settings() {
   const user = useParams();
-  const [foundUserInfo, setFoundUserInfo] = useState(null)
-  const [rerender, setRerender] = useState(false)
+  const navigate = useNavigate();
+  const [foundUserInfo, setFoundUserInfo] = useState(null);
+  const [rerender, setRerender] = useState(false);
 
   useEffect(() => {
     const getUserInfo = async () => {
-      const userInfo = await axios.post("http://localhost:3500/user/otherUser", {
-        user: user.user,
-      });
-       await setFoundUserInfo(userInfo.data);
+      try {
+        const userInfo = await axios.post(
+          "http://localhost:3500/user/otherUser",
+          {
+            user: user.user,
+          }
+        );
+        setFoundUserInfo(userInfo.data)
+      } catch (err) {
+        console.error(err);
+        navigate("/404");
+      }
     };
-    getUserInfo()
-  },[rerender]);
-  
+    getUserInfo();
+  }, [rerender]);
+
+  const handleLogout = async () => {
+    const response = await axios.delete("http://localhost:3500/logout", {
+      withCredentials: true,
+    });
+    navigate("/");
+  };
   return (
     <div className="settingsContainer">
       <div className="settingsTitle">
-        <h1>{foundUserInfo?foundUserInfo.username:""}'s Profile</h1>
+        <h1>{foundUserInfo ? foundUserInfo.username : ""}'s Profile</h1>
       </div>
 
       <div className="settingsSidebar">
         <ul className="settings">
-          <li>Profile</li>
-          <Link to="/dashboard" ><li>Dashboard</li></Link>
-          <Link to="/" className="logout"><li>Logout</li></Link>
+          <Link to="/dashboard">
+            <li className="dashboard">Dashboard</li>
+          </Link>
+          <li className="logout" onClick={handleLogout}>
+            Logout
+          </li>
         </ul>
       </div>
-      <Profile foundUserInfo={foundUserInfo?foundUserInfo:""} rerender={rerender} setRerender={setRerender}/>
-      
+      <Profile
+        foundUserInfo={foundUserInfo ? foundUserInfo : ""}
+        rerender={rerender}
+        setRerender={setRerender}
+      />
     </div>
   );
 }
